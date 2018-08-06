43d3a40f6ed70ec584c93f4ee85b892bcfb3f2f7
palslav <pallavchoudhary07@gmail.com>
2018-08-06 23:47:42 +0530

    Adding Design - 3 Bluetooth Show All Devices and Paired Devices implementation.

--- app/src/main/java/com/github/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/github/bluetoothchat/MainActivity.java
@@ -1,19 +1,37 @@
 package com.github.bluetoothchat;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonON, buttonOFF;
+    Button buttonON, buttonOFF, buttonToPaired, buttonShowAll;
     BluetoothAdapter myBluetoothAdapter;
     Intent btEnableIntent;
     int requestCodeForEnable;
+    ListView pairedListView, allDeviceListView;
+
+    ArrayAdapter arrayAdapter;
+    ArrayList<String> allDevicesList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
         buttonON = (Button) findViewById(R.id.btON);
         buttonOFF = (Button) findViewById(R.id.btOFF);
+        buttonToPaired = (Button) findViewById(R.id.btToPaired);
+        buttonShowAll = (Button) findViewById(R.id.btShowAll);
+
+        //pairedListView = (ListView) findViewById(R.id.allDevicesListView);
+        allDeviceListView = (ListView) findViewById(R.id.allDevicesListView);
 
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -28,13 +51,53 @@ public class MainActivity extends AppCompatActivity {
 
         bluetoothONMethod();
         bluetoothOFFMethod();
+        bluetoothShowAllMethod();
+        bluetoothToPairedMethod();
     }
+
+    private void bluetoothToPairedMethod() {
+        buttonToPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PairedDevices.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bluetoothShowAllMethod() {
+        buttonShowAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //showExplanation()
+                myBluetoothAdapter.startDiscovery();
+            }
+        });
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+        registerReceiver(myBroadcastReceiver, intentFilter);
+
+        arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, allDevicesList);
+        allDeviceListView.setAdapter(arrayAdapter);
+    }
+
+    BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                allDevicesList.add(device.getName());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
 
     private void bluetoothOFFMethod() {
         buttonOFF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(myBluetoothAdapter.isEnabled()){
+                if (myBluetoothAdapter.isEnabled()) {
                     myBluetoothAdapter.disable();
                     Toast.makeText(getApplicationContext(), "Bluetooth is Disabled", Toast.LENGTH_LONG).show();
                 }
@@ -44,10 +107,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==requestCodeForEnable){
-            if(resultCode==RESULT_OK){
+        if (requestCode == requestCodeForEnable) {
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Bluetooth is Enabled", Toast.LENGTH_LONG).show();
-            } else if(resultCode==RESULT_CANCELED){
+            } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(getApplicationContext(), "Unable to Enable Bluetooth", Toast.LENGTH_LONG).show();
             }
         }
@@ -57,14 +120,42 @@ public class MainActivity extends AppCompatActivity {
         buttonON.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(myBluetoothAdapter==null){
+                if (myBluetoothAdapter == null) {
                     Toast.makeText(getApplicationContext(), " Bluetooth is not supported", Toast.LENGTH_LONG).show();
                 } else {
-                    if(!myBluetoothAdapter.isEnabled()){
+                    if (!myBluetoothAdapter.isEnabled()) {
                         startActivityForResult(btEnableIntent, requestCodeForEnable);
                     }
                 }
             }
         });
+    }
+
+    private final int REQUEST_PERMISSION_ACCESS_COARSE_LOCATION =1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void showExplanation(String title, String message, final String permission, final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title).setMessage(message).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                requestPermission(permission, permissionRequestCode);
+            }
+        });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{permissionName}, permissionRequestCode);
     }
 }
